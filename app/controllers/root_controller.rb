d644ade7727a2a5b76a35f1692cf0221122b4c8c
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2015-12-12 17:26:20 +0000

    Added RootController.

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,14 @@
+class RootController < ApplicationController
+
+  def index
+    paths  = []
+    routes = paths.collect { |path| get_domain + path }
+    render_json(routes)
+  end
+
+  private
+  def get_domain
+    request.protocol + request.host_with_port
+  end
+
+end
